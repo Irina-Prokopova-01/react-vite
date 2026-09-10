b36de41c975c182bd0cb9005af7b370934aeb260
Irina <Irina-prokopova.style@yandex.ru>
2026-09-09 18:52:00 -0700

    Various methods to right condition

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 const username = 'Irina'
-const isLoggedIn = false
+const isLoggedIn = true
 
 const App = () => {
     return (
-        <div>
+        <>
             <h1 className="title">To Do List</h1>
-            {isLoggedIn && <p>Hi, {username}!</p>}
-        </div>
+            <p>{isLoggedIn ? `Hi, ${username}!` : 'Please log in'}</p>
+            {isLoggedIn ? <p>Hi, {username}!</p> : <button>Log in</button>}
+        </>
     )
 }
 
